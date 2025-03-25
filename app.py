b26d81e0f35f8deb66b4b6c760c8298214460acf
Amila619 <amilasandeepa349@gmail.com>
2025-03-25 16:27:01 +0530

    Revert "- found out that vercel is stateless and traditional session storage (like filesystem sessions) won't work because files aren't persisted between requests."
    This reverts commit 698b46d5e914a55f77b552db8991e420e55655ea.

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, redirect, session
+from flask_session import Session
 from pydantic import ValidationError
 from database import get_db
 from schemas import User
 from datetime import timedelta
-import secrets
 
 app = Flask(__name__)
-
-app.secret_key = secrets.token_hex(32)
 app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = "filesystem"
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+
+Session(app)
 
 @app.route("/")
 def root():
